e9cb0bee229a3cc75a32cf4c3fe981c9e67275ca
Denis <103167349+Den1sizotov@users.noreply.github.com>
2023-01-17 16:40:43 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function isWebp() {
+  function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+  testWebP(function (support) {
+    let className = support === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+  });
+}
+isWebp()
+/*- navbar -*/
+function navBar() {
+  const burger = document.querySelector('#burger');
+  if (burger) {
+    const header = document.querySelector('.navbar');
+    burger.addEventListener('click', function () {
+      header.classList.toggle('active-menu')
+      document.body.classList.toggle('_lock')
+    })
+  }
+}
+navBar()
